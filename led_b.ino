796723f32eac75756cd047814584a0634f75cc1f
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-28 19:43:55 +0530

    Proteus Model and Arduino IDE code

---
+++ led_b.ino
@@ -0,0 +1,15 @@
+void setup() 
+{
+ pinMode(7, OUTPUT); // put your setup code here, to run once:
+ pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW);
+delay(1000);
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH);
+delay(1000);
+}
